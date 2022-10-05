62cc22d4363043918a003ff5c1104fae40a2b96a
Ktulxy <omudritky@gmail.com>
2022-10-05 23:02:52 +0200

    Распарсил,  нужно перенести в другой класс

--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -1,16 +1,25 @@
+import org.json.JSONObject;
+
 import static io.restassured.RestAssured.given;
 import static util.Constants.TOKEN;
 
 public class Connect {
     public static void auth(String url) {
-      Object responce = given().
+      Object resp = given().
                 when().
                 header("Authorization",TOKEN).
                 get(url).
-                then().extract().response().asString();
+
+               then().extract().response().asString();
+      System.out.println("Пишем коннект " + resp);
               //  assertThat().statusCode(200).
               //  log().all();
-        System.out.println("Пишем коннект " + responce);
 
+        String str = (String) resp;
+        JSONObject obj = new JSONObject(str);
+        String n = obj.getString("name");
+
+        System.out.println(n);
     }
 }
+
